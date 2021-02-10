5773b5a52c343326833b47ae940119e4b5f1095d
Ampex <kasperski.blazej@gmail.com>
2021-02-10 22:12:09 +0100

    refactor product list

---
+++ src/components/Products.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect, useContext } from "react"
+import { getProductList } from "api"
+import { CartContext } from "../context"
+import Product from "./Product"
+
+const Products = () => {
+  const [products, setProducts] = useContext(CartContext)
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+  const getProducts = async () => {
+    const products = await getProductList()
+    const addQuantity = products.map((item) => ({
+      ...item,
+      quantity: 1,
+    }))
+    const updatedList = addQuantity.map((item) => ({
+      ...item,
+      sum: parseFloat(item.price) * item.quantity,
+    }))
+    setProducts(updatedList)
+  }
+
+  const productList = products.map((product) => (
+    <Product key={product.pid} {...product} />
+  ))
+
+  return (
+    <div className="container py-3">
+      <h3 className="py-4">Lista produktów</h3>
+      {products.length ? (
+        <ul className="px-3">{productList}</ul>
+      ) : (
+        "Wczytywanie..."
+      )}
+    </div>
+  )
+}
+
+export default Products
