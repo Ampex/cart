7b71918ad6fe13412cba9c93a2fb7f5e6792e426
Ampex <kasperski.blazej@gmail.com>
2021-02-10 22:12:57 +0100

    refactor product component view

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,44 @@
-import React, { useState } from "react"
+import React, { useContext } from "react"
 import { Button } from "react-bootstrap"
+import { CartContext } from "../context"
 
-const Product = ({ name, price }) => {
-  const [count, setCount] = useState(1)
+const Product = ({ pid, name, price, quantity, isBlocked }) => {
+  const [products, setProducts] = useContext(CartContext)
+
+  const handleIncrease = () => {
+    setProducts(
+      [...products],
+      products[products.findIndex((item) => item.pid === pid)].quantity++
+    )
+  }
+  const handleDecrease = () => {
+    setProducts(
+      [...products],
+      [products[products.findIndex((item) => item.pid === pid)].quantity--]
+    )
+  }
+
   return (
-    <li className="row py-3 border-bottom">
-      <div className="col pl-0">
+    <li className="row py-3 shadow-sm bg-white rounded d-flex align-items-center">
+      <div className="col-sm-6 pl-0">
         <h5 className="col-12">{name}</h5>
         <span className="col-auto">Cena: {price} zł</span>
       </div>
-      <Button disabled={false}>-</Button>
-      <span>{count}</span>
-      <Button disabled={false}>+</Button>
+      <span className="col py-2 text-muted">
+        Obecnie masz {quantity} sztuk produktu
+      </span>
+      <div className="col-md-3">
+        <Button
+          onClick={handleDecrease}
+          className="m-md-2 mr-2"
+          disabled={isBlocked}
+        >
+          -
+        </Button>
+        <Button onClick={handleIncrease} className="" disabled={isBlocked}>
+          +
+        </Button>
+      </div>
     </li>
   )
 }
